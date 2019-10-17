abbbaf686e42d617dac55d1df4a67f931a92135e
Morgan 42 <mojacque@student.le-101.fr>
2019-10-17 14:12:22 +0200

    commit other function libft & fix functions

--- Programs/ft_putnbr_fd.c
+++ Programs/ft_putnbr_fd.c
@@ -6,17 +6,12 @@
 /*   By: mojacque <mojacque@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/17 12:07:48 by mojacque     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/17 12:23:08 by mojacque    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/17 14:06:24 by mojacque    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void		ft_putchar(int fd, char c)
-{
-	write(fd, &c, 1);
-}
 
 void			ft_putnbr_fd(int n, int fd)
 {
@@ -25,7 +20,7 @@ void			ft_putnbr_fd(int n, int fd)
 	i = n;
 	if (n < 0)
 	{
-		ft_putchar(fd, '-');
+		ft_putchar_fd('-', fd);
 		i = i * (-1);
 	}
 	if (i >= 10)
@@ -35,6 +30,6 @@ void			ft_putnbr_fd(int n, int fd)
 	}
 	else if (i < 10)
 	{
-		ft_putchar(fd, i + '0');
+		ft_putchar_fd(i + '0', fd);
 	}
 }
