d6a810dc2a4cb8471dbc7b1945a1389b16bdcc81
Morgan 42 <mojacque@student.le-101.fr>
2019-11-14 19:56:59 +0100

    fix functions ft_strmapi.c

--- Programs/ft_strmapi.c
+++ Programs/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: mojacque <mojacque@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/01 15:40:32 by mojacque     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/01 15:50:49 by mojacque    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/14 19:55:17 by mojacque    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,10 +18,10 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*dest;
 	unsigned int	i;
 
-	if (s != NULL || f != NULL)
+	if (s == NULL || f == NULL)
 		return (NULL);
 	dest = malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (dest != NULL)
+	if (dest == NULL)
 		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
