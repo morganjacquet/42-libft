faea67e4209ce2454bfe4b71e6d4caaa7a04c57d
Morgan 42 <mojacque@student.le-101.fr>
2019-11-20 14:25:52 +0100

    fix ft_strncmp crash moulinette

--- Functions/ft_strncmp.c
+++ Functions/ft_strncmp.c
@@ -6,27 +6,21 @@
 /*   By: mojacque <mojacque@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/11 10:48:39 by mojacque     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/01 12:37:37 by mojacque    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/20 13:17:14 by mojacque    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-int		ft_strncmp(char *s1, char *s2, int n)
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int i;
+	size_t i;
 
 	i = 0;
-	while (s1[i] == s2[i])
-	{
-		if (s1[i] == '\0' || s2[i] == '\0')
-		{
-			return (0);
-		}
-		i++;
-	}
-	if (i + 1 > n)
-	{
+	if (n == 0)
 		return (0);
-	}
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i + 1 < n)
+		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
